609009a8884cac6ce8838bcdc0a12cd9635edbc4
arithmetician1 <sean_walker@outlook.com>
2016-11-23 22:18:16 -0500

    Remove debugging info.

--- static/scripts.js
+++ static/scripts.js
@@ -1,26 +1,22 @@
-var responses = ['No.', 'Not yet!', 'Nuh-uh.', 'Nope.', 'Doesn\'t look like it.', 'Nah.', 'Try again later.']
+var responses = [ 'No.', 'Not yet!', 'Nuh-uh.', 'Nope.', 'Doesn\'t look like it.', 'Nah.', 'Try again later.' ]
 
 $(function() {
     $( "#check-button" ).click(function() {
 
-        var wait = Math.floor((Math.random() + 1.1) * 1000);
-        // debugging
-        console.log(wait);
-        var index = Math.floor(Math.random() * 6);
+        var wait = Math.floor( ( Math.random() + 1.1 ) * 1000);
+        var index = Math.floor( Math.random() * 6 );
 
         $( "#check-result" ).html("<img src='static/ajax-loader.gif'>");
-        setTimeout(function(){
+        setTimeout( function() {
             $( "#check-result" ).html("<p>" + responses[index] + "</p>");
             $.getJSON( "/articles", function( data ) {
                 var items = [];
                 $.each( data, function( index, val ) {
                     items.push( "<a target='_blank' class='list-group-item' href='" + val.link.split("&url=")[1] + "'><h4 class='list-group-item-heading'>" + val.title.split(" - ")[0] + "</h4><p class='list-group-item-text'>" + val.title.split(" - ")[1] + "</p></a>" );
                 });
-                // debugging
-                console.log(items);
-                $( "#articles" ).html("<h3>Here are ten reasons why, courtesy of Google News:</h3><br><div class='list-group'>" + items.join("") + "</div>");
+                $( "#articles" ).html( "<h3>Here are ten reasons why, courtesy of Google News:</h3><br><div class='list-group'>" + items.join("") + "</div>" );
             }).fail( function( error ) {
-                console.log(error);
+                console.log( error );
             });
         }, wait);
     });
